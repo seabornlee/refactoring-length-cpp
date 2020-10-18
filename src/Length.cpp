d0433bf05d737c0c90a0f0acadfdb98d4ff9836f
Seaborn Lee <seabornlee.cn@gmail.com>
2020-10-19 00:45:55 +0800

    task1

--- src/Length.cpp
+++ src/Length.cpp
@@ -16,22 +16,26 @@ const char *Length::getUinnt() {
 
 Length Length::as(const char *u) {
     Length len = *this;
-    if (strcmp(this->uinnt, "f") == 0 && strcmp(u, "inch") == 0) len = Length(this->val * 12, u);
-    if (strcmp(this->uinnt, "inch") == 0 && strcmp(u, "f") == 0) len = Length(this->val / 12, u);
+    if (strcmp(this->uinnt, "f") == 0 && strcmp(u, "inch") == 0) {
+        len = Length(this->val * 12, u);
+    }
+    if (strcmp(this->uinnt, "inch") == 0 && strcmp(u, "f") == 0) {
+        len = Length(this->val / 12, u);
+    }
 
-        if (strcmp(this->uinnt, "yard") == 0) {
-            if (strcmp(u, "inch") == 0) {
-                len = Length(this->val * 36, u);
-            } else if (strcmp(u, "f") == 0) {
-                len = Length(this->val * 3, u);
-            }
-        } else if (strcmp(u, "yard") == 0) {
-            if (strcmp(this->uinnt, "f") == 0) {
-                len = Length(this->val / 3, u);
-            } else if (strcmp(this->uinnt, "inch") == 0) {
-                len = Length(this->val / 36, u);
-            }
+    if (strcmp(this->uinnt, "yard") == 0) {
+        if (strcmp(u, "inch") == 0) {
+            len = Length(this->val * 36, u);
+        } else if (strcmp(u, "f") == 0) {
+            len = Length(this->val * 3, u);
         }
+    } else if (strcmp(u, "yard") == 0) {
+        if (strcmp(this->uinnt, "f") == 0) {
+            len = Length(this->val / 3, u);
+        } else if (strcmp(this->uinnt, "inch") == 0) {
+            len = Length(this->val / 36, u);
+        }
+    }
     return len;
 }
 
